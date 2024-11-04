555110f48b66e4bffcd1bb32d995abf7fa718a54
minho2633 <minho2633@naver.com>
2024-11-04 13:55:03 +0900

    feat: 네이버 지도 api 통한 좌표to도로명주소 변환 기능 추가

--- frontend/lib/components/map/map_parking_submit.dart
+++ frontend/lib/components/map/map_parking_submit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
 
 class ParkingSubmit extends StatefulWidget {
@@ -17,7 +18,10 @@ class ParkingSubmit extends StatefulWidget {
 }
 
 class _ParkingSubmitState extends State<ParkingSubmit> {
-  String? address;
+  String? address1;
+  String? address2;
+  String? address3;
+  String? address4;
   bool loading = true;
 
   final TextEditingController addressController = TextEditingController();
@@ -31,34 +35,46 @@ class _ParkingSubmitState extends State<ParkingSubmit> {
   }
 
   Future<void> _fetchAddress() async {
-    final apiKey = 'YOUR_API_KEY'; // 여기에 본인의 API 키 입력
+    const naverApiId = '3i1rm7eji2'; // 여기에 네이버 API 키 입력
+    const naverApiKey = 'rXnUqjSKLFR8Q74ISQvysyxebUSDXNgfVA7N1U3U';
     final url =
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=${widget.latitude},${widget.longitude}&key=$apiKey';
+        'https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${widget.longitude},${widget.latitude}&output=json&orders=roadaddr';
 
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'x-ncp-apigw-api-key-id': naverApiId, // 클라이언트 ID
+          'x-ncp-apigw-api-key': naverApiKey, // 클라이언트 Secret
+        },
+      );
+
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        if (data['results'].isNotEmpty) {
+        print(data['results'][0]['land']);
+        if (data["results"][0].isNotEmpty) {
           setState(() {
-            address = data['results'][0]['formatted_address'];
+            address1 = data["results"][0]['region']['area1']['name']; // 도로명 주소
+            address2 = data["results"][0]['region']['area2']['name']; // 도로명 주소
+            address3 = data["results"][0]['region']['area3']['name']; // 도로명 주소
+            address4 = data["results"][0]['land']['name']; // 도로명 주소
             loading = false;
           });
         } else {
           setState(() {
-            address = '주소를 찾을 수 없습니다.';
+            address1 = '주소를 찾을 수 없습니다.';
             loading = false;
           });
         }
       } else {
         setState(() {
-          address = '주소 가져오기 실패';
+          address1 = '주소 가져오기 실패';
           loading = false;
         });
       }
     } catch (e) {
       setState(() {
-        address = '오류: $e';
+        address1 = '오류: $e';
         loading = false;
       });
     }
@@ -68,7 +84,7 @@ class _ParkingSubmitState extends State<ParkingSubmit> {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -78,30 +94,31 @@ class _ParkingSubmitState extends State<ParkingSubmit> {
             ),
             const SizedBox(height: 8),
             loading
-                ? CircularProgressIndicator() // 로딩 중일 때 인디케이터 표시
+                ? const CircularProgressIndicator() // 로딩 중일 때 인디케이터 표시
                 : Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '위도: ${widget.latitude}', // 위도 표시
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        '경도: ${widget.longitude}', // 경도 표시
-                        style: TextStyle(fontSize: 16),
+                        '${address1} ${address2} ${address3} ${address4}', // 현재 핀 위치
+                        style: const TextStyle(fontSize: 16),
                       ),
                       const SizedBox(height: 16),
                       TextField(
                         controller: addressController,
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                           labelText: '주소',
                           border: OutlineInputBorder(),
                         ),
+                        readOnly: true, // 주소 필드를 읽기 전용으로 설정
+                        onTap: () {
+                          addressController.text =
+                              '${address1} ${address2} ${address3} ${address4}'; // 가져온 주소 설정
+                        },
                       ),
                       const SizedBox(height: 16),
                       TextField(
                         controller: detailedAddressController,
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                           labelText: '상세 주소',
                           border: OutlineInputBorder(),
                         ),
