6b977b1ee0665f714c634426b11577c09cb1bab5
galler-ist <swghj@naver.com>
2024-11-07 03:38:37 +0900

    fix : update SecurityConfig since deprecated

--- backend/src/main/java/a102/PickingParking/config/SecurityConfig.java
+++ backend/src/main/java/a102/PickingParking/config/SecurityConfig.java
@@ -16,20 +16,33 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.cors().configurationSource(corsConfigurationSource()) // CORS 설정 적용
+//                .and().csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeHttpRequests()
+//                .requestMatchers(
+//                        // Swagger 허용 URL
+//                        "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
+//                        "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
+//                        "/webjars/**", "/swagger-ui.html"
+//                ).permitAll()
+//                .anyRequest().authenticated();
+//
+//        return http.build();
+//    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(corsConfigurationSource()) // CORS 설정 적용
-                .and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        // Swagger 허용 URL
-                        "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
-                        "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
-                        "/webjars/**", "/swagger-ui.html"
-                ).permitAll()
-                .anyRequest().authenticated();
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll() // 공개 경로 설정
+                        .anyRequest().authenticated() // 나머지 경로는 인증 요구
+                );
 
         return http.build();
     }
