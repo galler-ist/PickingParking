3f4c0cd0ec32a0533e21a9d5ddd913ace70874c6
galler-ist <swghj@naver.com>
2024-11-11 11:06:19 +0900

    feat : make point CHARGE and point PAYMENT

--- backend/src/main/java/a102/PickingParking/dto/PointRequestDto.java
+++ backend/src/main/java/a102/PickingParking/dto/PointRequestDto.java
@@ -1,5 +1,6 @@
 package a102.PickingParking.dto;
 
+import a102.PickingParking.entity.PointSource;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,9 @@ import lombok.Setter;
 public class PointRequestDto {
     private String userId;
     private int point_price;
-    private String point_source;
+    private PointSource point_source;
 
-    public PointRequestDto(String userId, int point_price, String point_source) {
+    public PointRequestDto(String userId, int point_price, PointSource point_source) {
         this.userId = userId;
         this.point_price = point_price;
         this.point_source = point_source;
--- backend/src/main/java/a102/PickingParking/service/PointService.java
+++ backend/src/main/java/a102/PickingParking/service/PointService.java
@@ -1,7 +1,9 @@
 package a102.PickingParking.service;
 
 import a102.PickingParking.dto.PointRequestDto;
+import a102.PickingParking.entity.Point;
 import a102.PickingParking.entity.User;
+import a102.PickingParking.repository.PointRepository;
 import a102.PickingParking.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +34,30 @@ public class PointService {
 
     // 포인트 충전과 사용
 
+    @Autowired
+    private PointRepository pointRepository;
+
     public void pointRequest(PointRequestDto request) {
         // user_id로 user_seq 조회
         User user = userRepository.findByUserId(request.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        // 현재 포인트 확인
+        int currentPoints = user.getPoint();
+
+        // 포인트 증가
+        user.setPoint(currentPoints + request.getPoint_price()); // 요청된 금액만큼 포인트 이동
+
+        // 거래 기록 저장
+        Point point = new Point();
+        point.setSeq(user.getSeq());
+        point.setPrice(request.getPoint_price());
+        point.setSource(request.getPoint_source());
+
+        // 거래 기록을 DB에 저장
+        pointRepository.save(point);
+
+        // 사용자 정보를 DB에 저장 (포인트 업데이트)
+        userRepository.save(user);
     }
 }
