0ef72115d0f5cdc0c87cf4a4b67697c81eacf4b7
김혁 <kimh0716@gmail.com>
2024-11-13 17:52:10 +0900

    feat : complete login and signup

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -14,40 +14,21 @@ class ApiService {
   String? baseUrl = dotenv.env['BASE_URL'];
   String? googleApi = dotenv.env['GOOGLE_API_KEY'];
 
-  Future<dynamic> signUp(
-      Map<String, dynamic> formData, File? vehicleImage) async {
+  Future<dynamic> signUp(Map<String, dynamic> formData) async {
     final url = Uri.parse('$baseUrl/api/user/signup');
     try {
-      var request = http.MultipartRequest('POST', url)
-        ..headers['Content-Type'] = 'application/json';
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(formData),
+      );
 
-      formData.forEach((key, value) {
-        if (value is String) {
-          request.fields[key] = value;
-        }
-      });
-
-      if (vehicleImage != null) {
-        final mimeType = lookupMimeType(vehicleImage.path);
-        final fileStream =
-            http.ByteStream(Stream.castFrom(vehicleImage.openRead()));
-        final length = await vehicleImage.length();
-
-        request.files.add(http.MultipartFile(
-          'car_image',
-          fileStream,
-          length,
-          filename: vehicleImage.path.split('/').last,
-          contentType: MediaType.parse(mimeType ?? 'application/octet-stream'),
-        ));
-      }
-
-      final response = await request.send();
       if (response.statusCode == 200) {
         return response.statusCode;
       } else {
-        final responseData = await response.stream.bytesToString();
-        return jsonDecode(responseData);
+        return jsonDecode(response.body);
       }
     } catch (e) {
       return e;
@@ -107,12 +88,11 @@ class ApiService {
         body: jsonEncode(formData),
       );
       if (response.statusCode == 200) {
-        final responseData = jsonDecode(response.body);
-        controller.accessToken.value = responseData['accessToken'];
+        // final responseData = jsonDecode(response.body);
+        // controller.accessToken.value = responseData['accessToken'];
         return response.statusCode;
       } else {
         final responseData = jsonDecode(response.body);
-        print(response.statusCode);
         print('$baseUrl/api/user/login');
         return responseData;
       }
