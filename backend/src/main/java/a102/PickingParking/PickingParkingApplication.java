fe40bb8ed988ef9c0bb9cae40f05e24bfde5be73
Ubuntu <ubuntu@ip-172-26-4-59.ap-northeast-2.compute.internal>
2024-11-18 10:21:28 +0900

    dkj

--- backend/src/main/java/a102/PickingParking/PickingParkingApplication.java
+++ backend/src/main/java/a102/PickingParking/PickingParkingApplication.java
@@ -3,7 +3,7 @@ package a102.PickingParking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 
 @EnableScheduling
@@ -11,7 +11,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PickingParkingApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+//  		Dotenv dotenv = Dotenv.load();
 //		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 //		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 //		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
