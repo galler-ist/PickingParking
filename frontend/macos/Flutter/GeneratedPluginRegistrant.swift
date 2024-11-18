bcb191848a254e0a4424fc2da5425490cb1a28af
김혁 <kimh0716@gmail.com>
2024-11-18 09:54:15 +0900

    remove: delete firebase

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import firebase_core
-import firebase_messaging
 import flutter_secure_storage_macos
 import geolocator_apple
 import path_provider_foundation
@@ -16,8 +14,6 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
